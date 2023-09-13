799a7acf710c589e1a687fad8afdf91461931c9b
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2023-09-13 20:59:45 +1000

    injected correct time and date for two cities using moment.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
+function updateTime() {
+  //Los Angeles
+  let losAngelesElement = document.querySelector("#los_angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
 
+  losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "hh:mm:ss:SS [<small>]A[</small>]"
+  );
+
+  //Costa Rica
+  let costaRicaElement = document.querySelector("#costa_rica");
+  let costaRicaDateElement = costaRicaElement.querySelector(".date");
+  let costaRicaTimeElement = costaRicaElement.querySelector(".time");
+
+  costaRicaTime = moment().tz("America/Costa_Rica");
+
+  costaRicaDateElement.innerHTML = costaRicaTime.format("MMMM Do YYYY");
+  costaRicaTimeElement.innerHTML = costaRicaTime.format(
+    "hh:mm:ss:SS [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1);
